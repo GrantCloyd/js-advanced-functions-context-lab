e3a5e7b42de262fa149b0239b31ff6f74e7d06d3
Grant Cloyd <dgcloyd@gmail.com>
2021-05-29 15:37:43 -0500

    commit tree cccb9dc60c9efeb3d9e6fa446533e269225ab982

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 /* Your Code Here */
 
+function createEmployeeRecord([strFirst, strLast, strTitle, payRate]) {
+   return {
+      firstName: strFirst,
+      familyName: strLast,
+      title: strTitle,
+      payPerHour: payRate,
+      timeInEvents: [],
+      timeOutEvents: [],
+   }
+}
+
+function createEmployeeRecords(arrofArr) {
+   let employeeRecords = []
+   for (arr of arrofArr) {
+      let result = createEmployeeRecord(arr)
+      employeeRecords.push(result)
+   }
+   return employeeRecords
+}
+
+function createTimeInEvent(timeString) {
+   return {
+      type: "TimeIn",
+      hour: timeString.slice(11),
+      date: timeString.slice(0, 10),
+   }
+}
+
+console.log(createTimeInEvent("2014-03-28 1030"))
+
+function createTimeOutEvent(timeString) {}
+
+let nestedArr = [
+   ["james", "plurp", "song man", 2],
+   ["wanda", "sanz", "genius", 100],
+]
+
+console.log(createEmployeeRecords(nestedArr))
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -10,13 +49,16 @@
  */
 
 let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+   let eligibleDates = this.timeInEvents.map(function (e) {
+      return e.date
+   })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+   let payable = eligibleDates.reduce(
+      function (memo, d) {
+         return memo + wagesEarnedOnDate.call(this, d)
+      }.bind(this),
+      0
+   ) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+   return payable
 }
